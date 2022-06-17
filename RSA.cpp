350057164241921962456b8161f47266b4031635
AlinaTelnova <105862476+AlinaTelnova@users.noreply.github.com>
2022-06-17 22:18:26 +0300

    Update RSA.cpp

--- RSA.cpp
+++ RSA.cpp
@@ -11,14 +11,18 @@ int sundaram(int n)
 	for (i = 1; 3 * i + 1 < n; i++)
 	{
 		for (j = 1; (k = i + j + 2 * i * j) < n && j <= i; j++)
+		{
 			a[k] = 1;
+		}
 	}
 	for (i = n - 1; i >= 1; i--)
+	{
 		if (a[i] == 0)
 		{
 			return (2 * i + 1);
 			break;
 		}
+	}
 	delete[] a;
 }
 int gcd(int a, int b)
@@ -44,17 +48,17 @@ int main()
 	int e, e_prime = 0;
 	while (e_prime != 1)
 	{
-		e= rand() % 100;
+		e = rand() % 100;
 		e_prime = gcd(e, ((p_prime - 1) * (q_prime - 1)));
 	}
 	unsigned int d = 0, d_prime = 0;
 	while (d_prime != 1)
 	{
-		d+= 1;
+		d += 1;
 		d_prime = (e * d) % ((p_prime - 1) * (q_prime - 1));
 	}
-	cout << n << ',' << e << " - Your open key"<< endl;
-	cout << n << ',' << d << " - Your private key"<< endl;
+	cout << n << ',' << e << " - Your open key" << endl;
+	cout << n << ',' << d << " - Your private key" << endl;
 	const int MAX = 100;
 	char* Text = new char[MAX];
 	cout << "Please enter the Text. Use <Enter> button when done." << endl;
@@ -63,7 +67,7 @@ int main()
 	unsigned int* Tdecrypt = new unsigned int[MAX];
 	int b = 301;
 	int c;
-	cout << endl << setw(5) << "Text" << setw(6) << "ASCII"<< setw(20) << "CryptoText/Block#" << setw(14) << "ASCIIdecrypt" << setw(14) << "Text decrypt" << endl<< " " << endl;
+	cout << endl << setw(5) << "Text" << setw(6) << "ASCII" << setw(20) << "CryptoText/Block#" << setw(14) << "ASCIIdecrypt" << setw(14) << "Text decrypt" << endl << " " << endl;
 	for (int j = 0; j < MAX; j++)
 	{
 		c = 1;
@@ -96,9 +100,7 @@ int main()
 	}
 	for (int j = 0; j < MAX; j++)
 	{
-		cout << setw(5) << Text[j] << setw(6) << static_cast<int>(Text[j]) <<
-			setw(20) << CryptoText[j] << setw(14) << Tdecrypt[j] << setw(14) <<
-			static_cast<char>(Tdecrypt[j]) << endl;
+		cout << setw(5) << Text[j] << setw(6) << static_cast<int>(Text[j]) <<setw(20) << CryptoText[j] << setw(14) << Tdecrypt[j] << setw(14) <<static_cast<char>(Tdecrypt[j]) << endl;
 	}
 	delete[] Text;
 	delete[] CryptoText;
